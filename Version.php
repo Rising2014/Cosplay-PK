6cffd1316f4dbff96d1df176deb263867dcd0a91
Rising2014 <winfred.you@gmail.com>
2020-07-10 18:45:16 +0800

    Update Version.php

--- Version.php
+++ Version.php
@@ -1,1 +1,1 @@
-InitializeSign,1.5.2,dingzhouche.com,
+InitializeSign,1.5.2,dingzhouche.com,http://www.dingzhouche.com/Files/cosplay-pk.apk,itms-services://?action=download-manifest&url=https://gitee.com/shenzhen_and_hebei/plistsave/raw/master/cpk.plist,
