f694ad7edd7de2c19b6a950a25c677e8ba04888d
Rising2014 <winfred.you@gmail.com>
2021-01-02 02:50:32 +0800

    Update InitialDataGP.php

--- InitialDataGP.php
+++ InitialDataGP.php
@@ -1,5 +1,5 @@
-InitializeSign,
-1.5.2,
-hotryona.com,
-https://www.hotryona.com/Files/cosplay-pk.apk,
-itms-services://?action=download-manifest&url=https://gitee.com/shenzhen_and_hebei/plistsave/raw/master/cpk.plist,
+InitializeSign
+1.5.2
+hotryona.com
+https://www.hotryona.com/Files/cosplay-pk.apk
+itms-services://?action=download-manifest&url=https://gitee.com/shenzhen_and_hebei/plistsave/raw/master/cpk.plist
