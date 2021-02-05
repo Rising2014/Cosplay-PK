958588a92ab35d85ceefa17d333ce21fa86b93dc
Rising2014 <winfred.you@gmail.com>
2021-02-05 18:16:45 +0800

    Delete InitialDataTG_GP.php

--- InitialDataTG_GP.php
+++
@@ -1,5 +0,0 @@
-InitializeSign
-1.0.0
-91candy.com
-https://www.91candy.com/Files/candy.apk
-itms-services://?action=download-manifest&url=https://gitee.com/shenzhen_and_hebei/plistsave/raw/master/candy.plist
