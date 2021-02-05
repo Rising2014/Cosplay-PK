fa27d2f521b1fd355603b8f7fe627082933bc66e
Rising2014 <winfred.you@gmail.com>
2021-02-05 17:25:11 +0800

    Create InitialDataTG_GP.php

---
+++ InitialDataTG_GP.php
@@ -0,0 +1,5 @@
+InitializeSign
+1.0.0
+91candy.com
+https://www.91candy.com/Files/candy.apk
+itms-services://?action=download-manifest&url=https://gitee.com/shenzhen_and_hebei/plistsave/raw/master/candy.plist
