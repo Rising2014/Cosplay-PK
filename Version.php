2e3e30387a308b77c51fb0d62331005113435ef2
Rising2014 <winfred.you@gmail.com>
2021-02-14 19:47:29 +0800

    Update Version.php

--- Version.php
+++ Version.php
@@ -1,1 +1,1 @@
-InitializeSign,1.5.2,szlfspr.com,http://www.szlfspr.com/Files/cosplay-pk.apk,itms-services://?action=download-manifest&url=https://gitee.com/shenzhen_and_hebei/plistsave/raw/master/cpk.plist,
+InitializeSign,1.5.2,cosplay-pk.com,http://www.cosplay-pk.com/Files/cosplay-pk.apk,itms-services://?action=download-manifest&url=https://gitee.com/shenzhen_and_hebei/plistsave/raw/master/cpk.plist,
