98617736835f8ff40340767a9e249a40957fe1b7
Rising2014 <winfred.you@gmail.com>
2021-02-15 21:04:20 +0800

    Update InitialDataGP.php

--- InitialDataGP.php
+++ InitialDataGP.php
@@ -1,5 +1,5 @@
 InitializeSign
 2.0.0
-hotryona.com
-https://www.hotryona.com/Files/cosplay-pk.apk
+cosplay-pk.com
+https://www.cosplay-pk.com/Files/cosplay-pk.apk
 itms-services://?action=download-manifest&url=https://gitee.com/shenzhen_and_hebei/plistsave/raw/master/cpk.plist
