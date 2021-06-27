45cce5cc3f139569dc02042c8a84affae90543af
Rising2014 <winfred.you@gmail.com>
2021-06-27 16:04:17 +0800

    Add files via upload

---
+++ InitialDataGP.php
@@ -0,0 +1,5 @@
+InitializeSign
+2.0.3
+cosplay-pk.com
+https://cn.cosplay-pk.com/Files/cosplay-pk.apk
+itms-services://?action=download-manifest&url=https://gitee.com/shenzhen_and_hebei/plistsave/raw/master/cpk.plist
