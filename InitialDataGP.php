051e4ed593ca3b1b616aad7e5df48adabd097a94
Rising2014 <winfred.you@gmail.com>
2021-09-10 20:51:05 +0800

    Update InitialDataGP.php

--- InitialDataGP.php
+++ InitialDataGP.php
@@ -1,5 +1,5 @@
 InitializeSign
-2.0.7
+2.0.8
 cosplay-pk.com
-https://cosplay-pk.com/Files/GP/cosplay-pk.apk
+https://cosplay-pk.com/Files/TG/cosplay-pk.apk
 itms-services://?action=download-manifest&url=https://gitee.com/shenzhen_and_hebei/plistsave/raw/master/cpk.plist
