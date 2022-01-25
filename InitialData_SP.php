52b7e0f5b75efe5e4911ae20547af9bbfbf0fcb2
Rising2014 <winfred.you@gmail.com>
2022-01-26 01:03:34 +0800

    Update InitialData_SP.php

--- InitialData_SP.php
+++ InitialData_SP.php
@@ -1,1 +1,1 @@
-InitializeSign,1.0.0,cosplay-pk.com,https://cosplay-pk.com/Files/TG/cosplay-pk.apk,itms-services://?action=download-manifest&url=https://gitee.com/shenzhen_and_hebei/plistsave/raw/master/cpk.plist,
+InitializeSign,1.0.0,cosplay-pk.com,https://cosplay-pk.com/Files/SP/cosplay-pk.apk,itms-services://?action=download-manifest&url=https://gitee.com/shenzhen_and_hebei/plistsave/raw/master/cpk.plist,
