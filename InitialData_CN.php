a4825bd10e7c4c8add10e09255a16cdad076ea7c
Rising2014 <winfred.you@gmail.com>
2022-06-14 23:21:57 +0800

    Update InitialData_CN.php

--- InitialData_CN.php
+++ InitialData_CN.php
@@ -1,1 +1,1 @@
-InitializeSign,2.0.4,cosplay-pk.com,https://cn.cosplay-pk.com/Files/cosplay-pk.apk,itms-services://?action=download-manifest&url=https://gitee.com/shenzhen_and_hebei/plistsave/raw/master/cpk.plist,
+InitializeSign,2.0.5,cosplay-pk.com,https://cn.cosplay-pk.com/Files/cosplay-pk.apk,itms-services://?action=download-manifest&url=https://gitee.com/shenzhen_and_hebei/plistsave/raw/master/cpk.plist,
